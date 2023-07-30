a6b14e5885e2fb6ca2b224f35b26041468763893
Bakare OLuwatobiloba <bakareoluwatobiloba13@gmail.com>
2023-07-30 16:27:31 +0000

    added trofyprototyperender to allowed hosts

--- commerce/settings.py
+++ commerce/settings.py
@@ -30,6 +30,7 @@ SECRET_KEY = '6ps8j!crjgrxt34cqbqn7x&b3y%(fny8k8nh21+qa)%ws3fh!q'
 DEBUG = True
 
 ALLOWED_HOSTS = [
+    'trofyprototyperender.onrender.com',
     '127.0.0.1',
     'trofy-4rb3.onrender.com',
     'localhost',
