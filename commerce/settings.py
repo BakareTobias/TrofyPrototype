162fd47ff44adaa73d8c733b026d74ba032ee193
Bakare OLuwatobiloba <bakareoluwatobiloba13@gmail.com>
2023-11-29 20:04:06 +0000

    same as last

--- commerce/settings.py
+++ commerce/settings.py
@@ -60,8 +60,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'livereload.middleware.LiveReloadScript',
-"""     'django.contrib.staticfiles.middleware.StaticFilesMiddleware',
- """
+
 ]
 
 ROOT_URLCONF = 'commerce.urls'
