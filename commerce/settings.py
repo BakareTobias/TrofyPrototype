de526e06d1f6ed72105fcff41148ffc35204ce16
Bakare Tobiloba <110624185+BakareTobias@users.noreply.github.com>
2023-11-29 17:45:01 +0000

    Update settings.py to remove text/javascript

--- commerce/settings.py
+++ commerce/settings.py
@@ -14,7 +14,8 @@ import os
 import mimetypes
 
 #ADD MIMETYPE JAVASCRIPT
-mimetypes.add_type("text/javascript",".js",True)
+mimetypes.add_type("text/css", ".css", True)
+mimetypes.add_type("application/javascript", ".js", True)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
